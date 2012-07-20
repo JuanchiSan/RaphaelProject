1c1c61d907b33dffa548ffef48f491d90914d61b
juan.avellaneda <juan.avellaneda@globant.com>
2012-07-20 17:05:46 -0300

    added circle view

---
+++ app/views/circle.view.js
@@ -0,0 +1,40 @@
+define(
+  [
+    'jquery',
+    'backbone',
+    'raphael',
+    'circleModel',
+    'config'
+  ],
+  function($ ,Backbone, Raphael, CircleModel) {
+    'use strict';
+    var CircleView = Backbone.View.extend({
+
+      el : '#figure_container',
+
+      model : CircleModel,
+
+      events: {
+
+      },
+
+      initialize : function(board) {
+        this.model = new CircleModel();
+        this.model.set({'x' : 10, 'y' : 10, 'circleRadius' : 10});
+        this.render(board);
+      },
+
+      render : function(board) {
+        var circle = board.paper.circle(this.model.get('x'), this.model.get('y'), this.model.get('circleRadius'));
+
+      },
+
+      addfigure : function(figure){
+
+      }
+
+    });
+
+    return CircleView;
+  }
+);
